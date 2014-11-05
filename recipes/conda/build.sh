1b865083875e9993adc8027980009ad4b228bc41
olaurino <olaurino@cfa.harvard.edu>
2014-11-05 11:18:56 -0500

    include conda recipe and upgrade Iris for conda

---
+++ recipes/conda/build.sh
@@ -0,0 +1,20 @@
+cp -r $RECIPE_DIR/../../* .
+
+IRIS=$PREFIX/opt/iris
+mkdir -p $IRIS
+
+mvn -DskipTests=true -Dsherpa=no package
+cd iris/target
+chmod u+x Iris
+cp Iris $IRIS/launch
+cp -r lib $IRIS
+cp *.jar $IRIS
+cp COPYING $IRIS
+cp LICENSE* $IRIS
+cp README.txt $IRIS
+cp -r examples $IRIS
+
+echo "$PREFIX/opt/iris/launch \$@" > iris
+chmod u+x iris
+mkdir -p $PREFIX/bin/
+cp iris $PREFIX/bin/
