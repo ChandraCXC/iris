043f8685505728e4c054e083b27962650dadff5f
olaurino <olaurino@cfa.harvard.edu>
2014-12-11 15:49:46 -0500

    initial GUI implementation

---
+++ sed-builder/src/main/java/cfa/vo/sed/science/stacker/NormalizationConfiguration.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cfa.vo.sed.science.stacker;
+
+/**
+ *
+ * @author olaurino
+ */
+public class NormalizationConfiguration {
+    
+}
