eef39713decdf748c5de8f35e24da2e93799a816
jbudynk <jbudynkiewicz@cfa.harvard.edu>
2015-04-20 15:16:28 -0400

    Fix #83. Updated About frame. Shows change in developement from VAO to SAO

--- iris/src/main/java/cfa/vo/iris/About.java
+++ iris/src/main/java/cfa/vo/iris/About.java
@@ -56,42 +56,52 @@ public class About extends javax.swing.JDialog {
         jLabel7 = new javax.swing.JLabel();
         jLabel8 = new javax.swing.JLabel();
         jLabel9 = new javax.swing.JLabel();
-        jLabel10 = new javax.swing.JLabel();
         jLabel11 = new javax.swing.JLabel();
+        jLabel12 = new javax.swing.JLabel();
+        jLabel13 = new javax.swing.JLabel();
 
-        jLabel1.setFont(new java.awt.Font("Lucida Grande", 1, 18)); // NOI18N
+        jLabel1.setFont(new java.awt.Font("Lucida Grande", 1, 18));
         jLabel1.setText("Iris v2.1");
         jLabel1.setName("jLabel1"); // NOI18N
 
+        jLabel2.setFont(new java.awt.Font("Lucida Grande", 0, 12)); // NOI18N
         jLabel2.setText("Virtual Astronomical Observatory");
         jLabel2.setName("jLabel2"); // NOI18N
 
-        jLabel3.setText("VAO Analysis Tool");
+        jLabel3.setText("A VO Analysis Tool");
         jLabel3.setName("jLabel3"); // NOI18N
 
         jLabel4.setText("for Spectral Energy Distributions");
         jLabel4.setName("jLabel4"); // NOI18N
 
-        jLabel5.setText("Developed at:");
+        jLabel5.setText("Copyright 2015");
         jLabel5.setName("jLabel5"); // NOI18N
 
-        jLabel6.setText("NASA/IPAC Extragalactic Database");
+        jLabel6.setFont(new java.awt.Font("Lucida Grande", 0, 12)); // NOI18N
+        jLabel6.setText(" NASA/IPAC Extragalactic Database");
         jLabel6.setName("jLabel6"); // NOI18N
 
+        jLabel7.setFont(new java.awt.Font("Lucida Grande", 0, 12)); // NOI18N
         jLabel7.setText("for the");
         jLabel7.setName("jLabel7"); // NOI18N
 
         jLabel8.setText("Smithsonian Astrophysical Observatory");
         jLabel8.setName("jLabel8"); // NOI18N
 
-        jLabel9.setText("Space Telescope Science Institute");
+        jLabel9.setFont(new java.awt.Font("Lucida Grande", 0, 12)); // NOI18N
+        jLabel9.setText(" Smithsonian Astrophysical Observatory");
         jLabel9.setName("jLabel9"); // NOI18N
-
-        jLabel10.setIcon(new ImageIcon(getClass().getResource("/vao.png")));
-        jLabel10.setName("jLabel10"); // NOI18N
 
         jLabel11.setIcon(new ImageIcon(getClass().getResource("/Iris_logo.png")));
         jLabel11.setName("jLabel11"); // NOI18N
+
+        jLabel12.setFont(new java.awt.Font("Lucida Grande", 0, 12)); // NOI18N
+        jLabel12.setText("Previously developed (2011-2014) by:");
+        jLabel12.setName("jLabel12"); // NOI18N
+
+        jLabel13.setFont(new java.awt.Font("Lucida Grande", 0, 12)); // NOI18N
+        jLabel13.setText(" Space Telescope Science Institute");
+        jLabel13.setName("jLabel13"); // NOI18N
 
         org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -100,52 +110,51 @@ public class About extends javax.swing.JDialog {
             .add(layout.createSequentialGroup()
                 .addContainerGap()
                 .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                    .add(jLabel6)
-                    .add(jLabel9)
-                    .add(jLabel8)
                     .add(jLabel1)
-                    .add(jLabel4)
-                    .add(jLabel3)
-                    .add(jLabel5)
-                    .add(jLabel7)
-                    .add(jLabel2))
-                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
                     .add(layout.createSequentialGroup()
+                        .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                            .add(jLabel3)
+                            .add(jLabel8)
+                            .add(jLabel5)
+                            .add(jLabel4)
+                            .add(jLabel12)
+                            .add(jLabel6)
+                            .add(jLabel9))
                         .add(18, 18, 18)
                         .add(jLabel11, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 318, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
-                    .add(layout.createSequentialGroup()
-                        .add(51, 51, 51)
-                        .add(jLabel10, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 252, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)))
+                    .add(jLabel13)
+                    .add(jLabel7)
+                    .add(jLabel2))
                 .addContainerGap(28, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
             .add(layout.createSequentialGroup()
-                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                .add(30, 30, 30)
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.TRAILING)
+                    .add(jLabel11, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 159, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
                     .add(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .add(jLabel11, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 159, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                        .add(18, 18, 18)
-                        .add(jLabel10, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 97, Short.MAX_VALUE))
-                    .add(layout.createSequentialGroup()
-                        .add(40, 40, 40)
                         .add(jLabel1)
-                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(jLabel3)
                         .add(5, 5, 5)
+                        .add(jLabel3)
+                        .add(4, 4, 4)
                         .add(jLabel4)
-                        .add(26, 26, 26)
+                        .add(18, 18, 18)
                         .add(jLabel5)
-                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                        .add(2, 2, 2)
                         .add(jLabel8)
-                        .add(2, 2, 2)
-                        .add(jLabel9)
-                        .add(2, 2, 2)
-                        .add(jLabel6)
-                        .add(26, 26, 26)
-                        .add(jLabel7)
+                        .add(18, 18, 18)
+                        .add(jLabel12)
                         .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(jLabel2)))
+                        .add(jLabel9)
+                        .add(4, 4, 4)
+                        .add(jLabel6)))
+                .add(2, 2, 2)
+                .add(jLabel13)
+                .add(10, 10, 10)
+                .add(jLabel7)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(jLabel2)
                 .addContainerGap())
         );
 
@@ -155,8 +164,9 @@ public class About extends javax.swing.JDialog {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel12;
+    private javax.swing.JLabel jLabel13;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
