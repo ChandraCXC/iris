a59430843a58517171e4993e92c294e491a6f264
Omar Laurino <olaurino@cfa.harvard.edu>
2015-11-10 09:50:17 -0500

    add copyright header

--- iris-common/src/main/java/cfa/vo/iris/units/spv/UnknownUnits.java
+++ iris-common/src/main/java/cfa/vo/iris/units/spv/UnknownUnits.java
@@ -1,3 +1,18 @@
+/**
+ * This code was originally included in Specview:
+ * www.stsci.edu/institute/software_hardware/specview
+ *
+ * and distributed under a BSD license,
+ * as described in the LICENSE.STScI. file.
+ *
+ * Copyright goes to the original authors.
+ *
+ * This code has then been embedded into Iris, modified
+ * and redistributed as
+ * described in the LICENSE.IRIS file.
+ * Copyright 2015 Smithsonian Astrophysical Observatory
+ *
+ **/
 package cfa.vo.iris.units.spv;
 
 import cfa.vo.iris.units.UnitsException;
