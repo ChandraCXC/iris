76bf41ade405bdcb9c4555e38b47408f80d1fa5a
Erik H <erik.holum@cfa.harvard.edu>
2015-11-16 15:11:39 -0500

    Adding Unit Tests for SEDStacker Adding an abstract parent for Stacker integration tests Moving integration tests to name specific files, leaving @ignore flags in place Added basic unit test coverage for SED stacker

--- sed-builder/src/test/java/cfa/vo/sed/science/stacker/SedStackerRedshifterIT.java
+++ sed-builder/src/test/java/cfa/vo/sed/science/stacker/SedStackerRedshifterIT.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2015 Smithsonian Astrophysical Observatory
- * <p/>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,12 +21,9 @@
  */
 package cfa.vo.sed.science.stacker;
 
-import cfa.vo.interop.SAMPController;
 import cfa.vo.interop.SAMPFactory;
 import cfa.vo.interop.SAMPMessage;
-import cfa.vo.iris.interop.SedSAMPController;
 import cfa.vo.iris.sed.ExtSed;
-
 import static cfa.vo.sed.science.stacker.SedStackerAttachments.REDSHIFT;
 
 import cfa.vo.iris.utils.Default;
@@ -36,10 +33,8 @@ import cfa.vo.sherpa.SherpaClient;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 import org.astrogrid.samp.Response;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -47,23 +42,11 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- *
+ * 
  * @author jbudynk
  */
-public class SedStackerRedshifterTest {
+public class SedStackerRedshifterIT extends AbstracSEDStackerIT {
     
-    private static final Logger logger = Logger.getLogger(SedStackerRedshifterTest.class.getName());
-
-    double[] x1;
-    double[] y1;
-    double[] yerr1;
-    double[] x2;
-    double[] y2;
-    double[] yerr2;
-    double[] x3;
-    double[] y3;
-    double[] yerr3;
-
     double[] controlY1;
     double[] controlX1;
     double[] controlYerr1;
@@ -71,75 +54,29 @@ public class SedStackerRedshifterTest {
     double[] controlX1WithExtraSegment;
     double[] controlY1WithExtraSegment;
 
-    SegmentPayload segment1;
-    SegmentPayload segment2;
-    SegmentPayload segment3;
-
     SedStackerRedshiftPayload payload;
-
-    private SAMPController controller;
     private SedStackerRedshifter redshifter;
 
     @Before
-    public void setUp() {
-        x1 = new double[]{1, 5, 10, 15, 50, 100};
-        y1 = new double[]{0.1, 0.5, 1.0, 1.5, 5.0, 10.0};
-        yerr1 = new double[]{0.01, 0.05, 0.1, 0.15, 0.5, 1.0};
+    public void before() throws Exception {
+        controlY1 = new double[] { 0.1, 0.5, 1.0, 1.5, 5.0, 10.0 };
+        controlX1 = new double[] { 0.90909091, 4.54545455, 9.09090909,
+                13.63636364, 45.45454545, 90.90909091 };
+        controlYerr1 = new double[] { 0.01, 0.05, 0.1, 0.15, 0.5, 1.0 };
+        controlX2 = new double[] { 1.66666667, 3.33333333, 4.16666667,
+                6.66666667, 8.33333333 };
 
-        x2 = new double[]{2, 4, 5, 8, 10};
-        y2 = new double[]{1, 2, 3, 4, 5};
-        yerr2 = new double[]{0.1, 0.2, 0.3, 0.4, 0.5};
-
-        x3 = new double[]{0.5, 1.5, 3.0, 5.0, 10.5, 21.0};
-        y3 = new double[]{5.0, 15.0, 7.0, 4.5, 13.5, 10.5};
-        yerr3 = new double[]{0.5, 1.5, 0.7, 0.45, 1.35, 1.05};
-
-        segment1 = (SegmentPayload) SAMPFactory.get(SegmentPayload.class);
-        segment1.setX(x1);
-        segment1.setY(y1);
-        segment1.setYerr(yerr1);
-        segment1.setZ(0.1);
-        segment1.setId("Sed1");
-
-        segment2 = (SegmentPayload) SAMPFactory.get(SegmentPayload.class);
-        segment2.setX(x2);
-        segment2.setY(y2);
-        segment2.setYerr(yerr2);
-        segment2.setZ(0.2);
-        segment2.setId("Sed2");
-
-        segment3 = (SegmentPayload) SAMPFactory.get(SegmentPayload.class);
-        segment3.setX(x3);
-        segment3.setY(y3);
-        segment3.setYerr(yerr3);
-        segment3.setZ(0.3);
-        segment3.setId("Sed3");
-
-        controlY1 = new double[]{0.1, 0.5, 1.0, 1.5, 5.0, 10.0};
-        controlX1 = new double[]{0.90909091, 4.54545455, 9.09090909, 13.63636364, 45.45454545, 90.90909091};
-        controlYerr1 = new double[]{0.01, 0.05, 0.1, 0.15, 0.5, 1.0};
-        controlX2 = new double[]{1.66666667, 3.33333333, 4.16666667, 6.66666667, 8.33333333};
-
-        controlX1WithExtraSegment = new double[]{0.90909091, 4.54545455, 9.09090909, 13.63636364, 18.181818182, 45.45454545, 90.90909091};
-        controlY1WithExtraSegment = new double[]{0.1, 0.5, 1.0, 1.5, 2.0, 5.0, 10.0};
+        controlX1WithExtraSegment = new double[] { 0.90909091, 4.54545455,
+                9.09090909, 13.63636364, 18.181818182, 45.45454545, 90.90909091 };
+        controlY1WithExtraSegment = new double[] { 0.1, 0.5, 1.0, 1.5, 2.0,
+                5.0, 10.0 };
     }
 
     @Ignore("need sherpa-samp running")
     @Test
     public void testRedshift() throws Exception {
-        // Start the SAMP controller
-        controller = new SedSAMPController("SEDStacker", "SEDStacker", this.getClass().getResource("/tools_tiny.png").toString());
-        controller.setAutoRunHub(false);
-        controller.start(false);
-
-        Thread.sleep(2000);
-
-        while (!controller.isConnected()) {
-            logger.info("waiting connection");
-            Thread.sleep(1000);
-        }
-
-        payload = (SedStackerRedshiftPayload) SAMPFactory.get(SedStackerRedshiftPayload.class);
+        payload = (SedStackerRedshiftPayload) SAMPFactory
+                .get(SedStackerRedshiftPayload.class);
 
         // Setup the stack payload
         payload.addSegment(segment1);
@@ -149,17 +86,20 @@ public class SedStackerRedshifterTest {
         payload.setZ0(0.0);
 
         // Setup and send SAMP message
-        SAMPMessage message = SAMPFactory.createMessage("stack.redshift", payload, SedStackerRedshiftPayload.class);
+        SAMPMessage message = SAMPFactory.createMessage("stack.redshift",
+                payload, SedStackerRedshiftPayload.class);
 
         SherpaClient client = new SherpaClient(controller);
 
-        Response rspns = controller.callAndWait(client.findSherpa(), message.get(), 10);
+        Response rspns = controller.callAndWait(client.findSherpa(),
+                message.get(), 10);
         if (client.isException(rspns)) {
             Exception ex = client.getException(rspns);
             throw ex;
         }
 
-        SedStackerRedshiftPayload response = (SedStackerRedshiftPayload) SAMPFactory.get(rspns.getResult(), SedStackerRedshiftPayload.class);
+        SedStackerRedshiftPayload response = (SedStackerRedshiftPayload) SAMPFactory
+                .get(rspns.getResult(), SedStackerRedshiftPayload.class);
 
         // get response values
         SegmentPayload seg = response.getSegments().get(0);
@@ -169,41 +109,22 @@ public class SedStackerRedshifterTest {
 
         // tests
         for (int i = 0; i < seg.getY().length; i++) {
-            assertEquals(controlY1[i], seg.getY()[i], 0.00001);
+            assertEquals(controlY1[i], seg.getY()[i], EPSILON);
         }
         for (int i = 0; i < seg.getY().length; i++) {
-            assertEquals(controlX1[i], seg.getX()[i], 0.00001);
+            assertEquals(controlX1[i], seg.getX()[i], EPSILON);
         }
         for (int i = 0; i < seg.getY().length; i++) {
-            assertEquals(controlYerr1[i], seg.getYerr()[i], 0.00001);
+            assertEquals(controlYerr1[i], seg.getYerr()[i], EPSILON);
         }
         for (int i = 0; i < seg2.getX().length; i++) {
-            assertEquals(controlX2[i], seg2.getX()[i], 0.00001);
+            assertEquals(controlX2[i], seg2.getX()[i], EPSILON);
         }
-
-        controller.stop();
-    }
-
-    @After
-    public void tearDown() {
     }
 
     @Ignore("need sherpa-samp running")
     @Test
     public void testRedshifter() throws Exception {
-
-        // Start the SAMP controller
-        controller = new SedSAMPController("SEDStacker", "SEDStacker", this.getClass().getResource("/tools_tiny.png").toString());
-        controller.setAutoRunHub(false);
-        controller.start(false);
-
-        Thread.sleep(2000);
-        
-        while (!controller.isConnected()) {
-            logger.info("waiting connection");
-            Thread.sleep(1000);
-        }
-
         ExtSed sed1 = new ExtSed("Sed1");
         ExtSed sed2 = new ExtSed("Sed2");
         ExtSed sed3 = new ExtSed("Sed3");
@@ -215,13 +136,14 @@ public class SedStackerRedshifterTest {
         seg1.setSpectralAxisUnits("Angstrom");
         seg1.setDataValues(yerr1, UTYPE.FLUX_STAT_ERROR);
         sed1.addSegment(seg1);
-//	Segment seg11 = new Segment();
-//	seg11.setFluxAxisValues(new double[] {2.0});
-//	seg11.setSpectralAxisValues(new double[] {20});
-//	seg11.setFluxAxisUnits("Jy");
-//	seg11.setSpectralAxisUnits("nm");
-//	seg11.setDataValues(yerr1, UTYPEs.FLUX_STAT_ERROR);
-//	sed1.addSegment(seg11);
+
+        // Segment seg11 = new Segment();
+        // seg11.setFluxAxisValues(new double[] {2.0});
+        // seg11.setSpectralAxisValues(new double[] {20});
+        // seg11.setFluxAxisUnits("Jy");
+        // seg11.setSpectralAxisUnits("nm");
+        // seg11.setDataValues(yerr1, UTYPEs.FLUX_STAT_ERROR);
+        // sed1.addSegment(seg11);
         sed1.addAttachment(REDSHIFT, 0.1);
 
         Segment seg2 = new Segment();
@@ -273,11 +195,13 @@ public class SedStackerRedshifterTest {
             double[] x = xs.get(j);
             double[] y = ys.get(j);
 
-            for (int i = 0; i < stack.getOrigSeds().get(j).getSegment(0).getLength(); i++) {
-                double xOrigValue = origSed.getSegment(0).getSpectralAxisValues()[i];
+            for (int i = 0; i < stack.getOrigSeds().get(j).getSegment(0)
+                    .getLength(); i++) {
+                double xOrigValue = origSed.getSegment(0)
+                        .getSpectralAxisValues()[i];
                 double yOrigValue = origSed.getSegment(0).getFluxAxisValues()[i];
-                assertEquals(xOrigValue, x[i]);
-                assertEquals(yOrigValue, y[i]);
+                assertEquals(xOrigValue, x[i], EPSILON);
+                assertEquals(yOrigValue, y[i], EPSILON);
             }
         }
 
@@ -287,35 +211,21 @@ public class SedStackerRedshifterTest {
         for (int i = 0; i < shiftedSed1.getSegment(0).getLength(); i++) {
             double xValue = shiftedSed1.getSegment(0).getSpectralAxisValues()[i];
             double yValue = shiftedSed1.getSegment(0).getFluxAxisValues()[i];
-            assertEquals(controlX1[i], xValue, 0.00001);
-            assertEquals(controlY1[i], yValue, 0.00001);
-            assertEquals(controlYerr1[i], yerrValues[i], 0.00001);
+            assertEquals(controlX1[i], xValue, EPSILON);
+            assertEquals(controlY1[i], yValue, EPSILON);
+            assertEquals(controlYerr1[i], yerrValues[i], EPSILON);
 
         }
         ExtSed shiftedSed2 = stack.getSeds().get(1);
         for (int i = 0; i < shiftedSed2.getSegment(0).getLength(); i++) {
             double xValue = shiftedSed2.getSegment(0).getSpectralAxisValues()[i];
-            assertEquals(xValue, controlX2[i], 0.00001);
+            assertEquals(xValue, controlX2[i], EPSILON);
         }
-        controller.stop();
     }
 
     @Ignore("need sherpa-samp running")
     @Test
     public void testRedshifterNoZ() throws Exception {
-
-        // Start the SAMP controller
-        controller = new SedSAMPController("SEDStacker", "SEDStacker", this.getClass().getResource("/tools_tiny.png").toString());
-        controller.setAutoRunHub(false);
-        controller.start(false);
-
-        Thread.sleep(2000);
-
-        while (!controller.isConnected()) {
-            logger.info("waiting connection");
-            Thread.sleep(1000);
-        }
-
         ExtSed sed1 = new ExtSed("Sed1");
         ExtSed sed2 = new ExtSed("Sed2");
         ExtSed sed3 = new ExtSed("Sed3");
@@ -376,11 +286,13 @@ public class SedStackerRedshifterTest {
             ExtSed origSed = stack.getOrigSeds().get(j);
             double[] x = xs.get(j);
             double[] y = ys.get(j);
-            for (int i = 0; i < stack.getOrigSeds().get(j).getSegment(0).getLength(); i++) {
-                double xOrigValue = origSed.getSegment(0).getSpectralAxisValues()[i];
+            for (int i = 0; i < stack.getOrigSeds().get(j).getSegment(0)
+                    .getLength(); i++) {
+                double xOrigValue = origSed.getSegment(0)
+                        .getSpectralAxisValues()[i];
                 double yOrigValue = origSed.getSegment(0).getFluxAxisValues()[i];
-                assertEquals(xOrigValue, x[i]);
-                assertEquals(yOrigValue, y[i]);
+                assertEquals(xOrigValue, x[i], EPSILON);
+                assertEquals(yOrigValue, y[i], EPSILON);
             }
         }
 
@@ -390,18 +302,17 @@ public class SedStackerRedshifterTest {
         for (int i = 0; i < shiftedSed1.getSegment(0).getLength(); i++) {
             double xValue = shiftedSed1.getSegment(0).getSpectralAxisValues()[i];
             double yValue = shiftedSed1.getSegment(0).getFluxAxisValues()[i];
-            assertEquals(controlX1[i], xValue, 0.00001);
-            assertEquals(controlY1[i], yValue, 0.00001);
-            assertEquals(controlYerr1[i], yerrValues[i], 0.00001);
+            assertEquals(controlX1[i], xValue, EPSILON);
+            assertEquals(controlY1[i], yValue, EPSILON);
+            assertEquals(controlYerr1[i], yerrValues[i], EPSILON);
 
         }
-        // since sed2 doesn't have a redshift, the spectral values shouldn't change.
+        // since sed2 doesn't have a redshift, the spectral values shouldn't
+        // change.
         ExtSed shiftedSed2 = stack.getSeds().get(1);
         for (int i = 0; i < shiftedSed2.getSegment(0).getLength(); i++) {
             double xValue = shiftedSed2.getSegment(0).getSpectralAxisValues()[i];
-            assertEquals(xValue, x2[i], 0.00001);
+            assertEquals(xValue, x2[i], EPSILON);
         }
-        controller.stop();
     }
-
 }
