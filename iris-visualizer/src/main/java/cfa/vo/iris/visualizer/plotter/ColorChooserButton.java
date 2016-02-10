6ebb699df4870045beef403c5f3be4f52ab7ca8d
Jamie Budynkiewicz <jbudynkiewicz@cfa.harvard.edu>
2016-02-10 13:03:04 -0500

    Adds basic segment preferences widget prototype. No getters/setters or properties implemented yet.

---
+++ iris-visualizer/src/main/java/cfa/vo/iris/visualizer/plotter/ColorChooserButton.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2016 Chandra X-Ray Observatory.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cfa.vo.iris.visualizer.plotter;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JColorChooser;
+import javax.swing.JDialog;
+
+/**
+ *
+ * @author jbudynk
+ * 
+ * A JButton extension which opens a JColorChooser and displays a rectangle 
+ * of the selected color as the label of the button.
+ * 
+ * Adopted from 
+ * http://stackoverflow.com/questions/26565166/how-to-display-a-color-selector-when-clicking-a-button
+ */
+public class ColorChooserButton extends JButton {
+
+    private Color current;
+    private JColorChooser colorChooser;
+
+    public ColorChooserButton(Color c) {
+        setSelectedColor(c);
+        colorChooser = new JColorChooser();
+        
+        addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                JDialog dialog = colorChooser.createDialog(null, "Choose a color", true, colorChooser, okActionListener, null);
+                dialog.setVisible(true);
+            }
+        });
+    }
+
+    public Color getSelectedColor() {
+        return current;
+    }
+
+    public void setSelectedColor(Color newColor) {
+        setSelectedColor(newColor, true);
+    }
+
+    public void setSelectedColor(Color newColor, boolean notify) {
+
+        if (newColor == null) return;
+
+        current = newColor;
+        setIcon(createIcon(current, 32, 16));
+        repaint();
+
+        if (notify) {
+            // Notify everybody that may be interested.
+            for (ColorChangedListener l : listeners) {
+                l.colorChanged(newColor);
+            }
+        }
+    }
+
+    public static interface ColorChangedListener {
+        public void colorChanged(Color newColor);
+    }
+
+    private List<ColorChangedListener> listeners = new ArrayList<ColorChangedListener>();
+
+    public void addColorChangedListener(ColorChangedListener toAdd) {
+        listeners.add(toAdd);
+    }
+
+    public static  ImageIcon createIcon(Color main, int width, int height) {
+        BufferedImage image = new BufferedImage(width, height, java.awt.image.BufferedImage.TYPE_INT_RGB);
+        Graphics2D graphics = image.createGraphics();
+        graphics.setColor(main);
+        graphics.fillRect(0, 0, width, height);
+        graphics.setXORMode(Color.DARK_GRAY);
+        graphics.drawRect(0, 0, width-1, height-1);
+        image.flush();
+        ImageIcon icon = new ImageIcon(image);
+        return icon;
+    }
+    
+    ActionListener okActionListener = new ActionListener() {
+        public void actionPerformed(ActionEvent actionEvent) {
+            setSelectedColor(colorChooser.getColor());
+        }
+    };
+}
---
+++ iris-visualizer/src/main/java/cfa/vo/iris/visualizer/plotter/MarkSize.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2016 Chandra X-Ray Observatory.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cfa.vo.iris.visualizer.plotter;
+
+/**
+ *
+ * @author jbudynk
+ */
+public enum MarkSize {
+    one("1"),
+    two("2"),
+    three("3"),
+    four("4"),
+    five("5"),
+    six("6"),
+    seven("7"),
+    eight("8"),
+    nine("9"),
+    ten("10");
+    
+    private final String name;       
+
+    private MarkSize(String s) {
+        name = s;
+    }
+
+    public boolean equalsName(String otherName) {
+        return (otherName == null) ? false : name.equals(otherName);
+    }
+
+    public String toString() {
+       return this.name;
+    }
+}
