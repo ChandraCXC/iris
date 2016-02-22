df06bb74f26ef790ba67f4487d85cac2ba751792
Jamie Budynkiewicz <jbudynkiewicz@cfa.harvard.edu>
2016-02-22 17:35:44 -0500

    Add components to PlotPreferencesPanel. No properties management yet.

--- iris-visualizer/src/main/java/cfa/vo/iris/visualizer/preferences/PlotPreferencesPanel.java
+++ iris-visualizer/src/main/java/cfa/vo/iris/visualizer/preferences/PlotPreferencesPanel.java
@@ -15,17 +15,30 @@
  */
 package cfa.vo.iris.visualizer.preferences;
 
+import cfa.vo.iris.visualizer.plotter.PlotPreferences;
+
 /**
  *
  * @author jbudynk
  */
 public class PlotPreferencesPanel extends javax.swing.JPanel {
 
+    final private PlotPreferences prefs;
+    
     /**
      * Creates new form PlotPreferencesPanel
+     * @param prefs global plot preferences
      */
-    public PlotPreferencesPanel() {
+    public PlotPreferencesPanel(PlotPreferences prefs) {
+        
+        this.prefs = prefs;
+        
         initComponents();
+        
+        // plot range components
+        boolean plotRangeOptionsVisible = this.prefs.getFixed();
+        fixedCheckBox.setSelected(plotRangeOptionsVisible);
+        setPlotRangeOptionsVisible(fixedCheckBox.isSelected());
     }
 
     /**
@@ -36,20 +49,215 @@ public class PlotPreferencesPanel extends javax.swing.JPanel {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+        bindingGroup = new org.jdesktop.beansbinding.BindingGroup();
+
+        gridLinesButtonGroup = new javax.swing.ButtonGroup();
+        plotTypeLabel = new javax.swing.JLabel();
+        gridLinesLabel = new javax.swing.JLabel();
+        plotRangeLabel = new javax.swing.JLabel();
+        showErrorsCheckBox = new javax.swing.JCheckBox();
+        plotTypeComboBox = new javax.swing.JComboBox();
+        gridLinesOnRadioButton = new javax.swing.JRadioButton();
+        gridLinesOffRadioButton = new javax.swing.JRadioButton();
+        fixedCheckBox = new javax.swing.JCheckBox();
+        jPanel1 = new javax.swing.JPanel();
+        xminLabel = new javax.swing.JLabel();
+        yminTextField = new javax.swing.JTextField();
+        ymaxTextField = new javax.swing.JTextField();
+        xmaxTextField = new javax.swing.JTextField();
+        ymaxLabel = new javax.swing.JLabel();
+        xminTextField = new javax.swing.JTextField();
+        yminLabel = new javax.swing.JLabel();
+        xmaxLabel = new javax.swing.JLabel();
+        xunitComboBox = new javax.swing.JComboBox();
+        yunitComboBox = new javax.swing.JComboBox();
+
+        plotTypeLabel.setText("Plot type:");
+
+        gridLinesLabel.setText("Grid lines:");
+
+        plotRangeLabel.setText("Plot range:");
+
+        showErrorsCheckBox.setText("Show errors");
+
+        plotTypeComboBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Log", "Linear", "XLog", "YLog" }));
+
+        org.jdesktop.beansbinding.Binding binding = org.jdesktop.beansbinding.Bindings.createAutoBinding(org.jdesktop.beansbinding.AutoBinding.UpdateStrategy.READ_WRITE, this, org.jdesktop.beansbinding.ELProperty.create("${prefs}"), plotTypeComboBox, org.jdesktop.beansbinding.BeanProperty.create("selectedItem"));
+        bindingGroup.addBinding(binding);
+
+        gridLinesButtonGroup.add(gridLinesOnRadioButton);
+        gridLinesOnRadioButton.setText("On");
+
+        gridLinesButtonGroup.add(gridLinesOffRadioButton);
+        gridLinesOffRadioButton.setText("Off");
+
+        fixedCheckBox.setText("Automatic");
+        fixedCheckBox.setEnabled(false);
+
+        jPanel1.setVisible(false);
+
+        xminLabel.setText("xmin:");
+
+        yminTextField.setText("jTextField2");
+
+        ymaxTextField.setText("jTextField4");
+
+        xmaxTextField.setText("jTextField3");
+
+        ymaxLabel.setText("ymax:");
+
+        xminTextField.setText("jTextField1");
+
+        yminLabel.setText("ymin:");
+
+        xmaxLabel.setText("xmax:");
+
+        xunitComboBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
+        yunitComboBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(yminLabel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(yminTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(xminLabel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(xminTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(ymaxLabel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(ymaxTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(xmaxLabel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(xmaxTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(yunitComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(xunitComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(xminLabel)
+                    .addComponent(xminTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(xunitComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(xmaxLabel)
+                    .addComponent(xmaxTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(yminLabel)
+                    .addComponent(yminTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(yunitComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(ymaxLabel)
+                    .addComponent(ymaxTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap())
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(showErrorsCheckBox)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(plotTypeLabel)
+                            .addComponent(gridLinesLabel))
+                        .addGap(58, 58, 58)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(gridLinesOnRadioButton)
+                                .addGap(18, 18, 18)
+                                .addComponent(gridLinesOffRadioButton))
+                            .addComponent(plotTypeComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(plotRangeLabel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(fixedCheckBox))
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(plotTypeLabel)
+                    .addComponent(plotTypeComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(gridLinesLabel)
+                    .addComponent(gridLinesOnRadioButton)
+                    .addComponent(gridLinesOffRadioButton))
+                .addGap(18, 18, 18)
+                .addComponent(showErrorsCheckBox)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(plotRangeLabel)
+                    .addComponent(fixedCheckBox))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(45, Short.MAX_VALUE))
         );
+
+        bindingGroup.bind();
     }// </editor-fold>//GEN-END:initComponents
 
+    /**
+     * set the plot range options visible. Should be done when plot range is
+     * fixed.
+     */
+    private void setPlotRangeOptionsVisible(boolean visible) {
+        xmaxLabel.setVisible(visible);
+        xminLabel.setVisible(visible);
+        xunitComboBox.setVisible(visible);
+        xminTextField.setVisible(visible);
+        xmaxTextField.setVisible(visible);
+        
+        ymaxLabel.setVisible(visible);
+        yminLabel.setVisible(visible);
+        ymaxTextField.setVisible(visible);
+        yminTextField.setVisible(visible);
+        yunitComboBox.setVisible(visible);
+   }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JCheckBox fixedCheckBox;
+    private javax.swing.ButtonGroup gridLinesButtonGroup;
+    private javax.swing.JLabel gridLinesLabel;
+    private javax.swing.JRadioButton gridLinesOffRadioButton;
+    private javax.swing.JRadioButton gridLinesOnRadioButton;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel plotRangeLabel;
+    private javax.swing.JComboBox plotTypeComboBox;
+    private javax.swing.JLabel plotTypeLabel;
+    private javax.swing.JCheckBox showErrorsCheckBox;
+    private javax.swing.JLabel xmaxLabel;
+    private javax.swing.JTextField xmaxTextField;
+    private javax.swing.JLabel xminLabel;
+    private javax.swing.JTextField xminTextField;
+    private javax.swing.JComboBox xunitComboBox;
+    private javax.swing.JLabel ymaxLabel;
+    private javax.swing.JTextField ymaxTextField;
+    private javax.swing.JLabel yminLabel;
+    private javax.swing.JTextField yminTextField;
+    private javax.swing.JComboBox yunitComboBox;
+    private org.jdesktop.beansbinding.BindingGroup bindingGroup;
     // End of variables declaration//GEN-END:variables
 }
