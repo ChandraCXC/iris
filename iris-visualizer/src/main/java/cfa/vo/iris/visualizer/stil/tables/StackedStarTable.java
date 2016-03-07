60b8db6a0f37e9bf2a5d261ef2d1cebebb45e698
Erik <eholum@gmail.com>
2016-03-06 22:12:08 -0500

    Adding StackedStarTable implementation.

---
+++ iris-visualizer/src/main/java/cfa/vo/iris/visualizer/stil/tables/StackedStarTable.java
@@ -0,0 +1,34 @@
+package cfa.vo.iris.visualizer.stil.tables;
+
+import java.io.IOException;
+import java.util.List;
+
+import uk.ac.starlink.table.ConcatStarTable;
+import uk.ac.starlink.table.StarTable;
+import uk.ac.starlink.table.WrapperStarTable;
+
+/**
+ * Extension of a ConcatStarTable that supports stacking of any StarTable, regardless
+ * of ColumnInfo compatibility.
+ *
+ */
+public class StackedStarTable extends WrapperStarTable {
+    
+    private ColumnMetadataStarTable metadataTable;
+    private ColumnMappingStarTable[] dataTables;
+
+    public StackedStarTable(List<? extends StarTable> tables, ColumnMatcher matcher) 
+            throws IOException 
+    {
+        super(null);
+        
+        this.metadataTable = new ColumnMetadataStarTable(tables, matcher);
+        this.dataTables = new ColumnMappingStarTable[tables.size()];
+        
+        for (int i=0; i<tables.size(); i++) {
+            dataTables[i] = new ColumnMappingStarTable(tables.get(i), metadataTable, matcher);
+        }
+        
+        this.baseTable = new ConcatStarTable(metadataTable, dataTables);
+    }
+}
