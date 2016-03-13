f01675d247235dd205b16705811e067ad9c615c5
Erik <eholum@gmail.com>
2016-03-13 18:00:58 -0400

    Extract values from filtered Iris StarTables.

--- iris-visualizer/src/main/java/cfa/vo/iris/visualizer/filters/NullFilter.java
+++ iris-visualizer/src/main/java/cfa/vo/iris/visualizer/filters/NullFilter.java
@@ -4,11 +4,22 @@ import java.util.BitSet;
 
 import cfa.vo.iris.visualizer.stil.tables.IrisStarTable;
 
-public class NullFilter implements Filter {
+public class NullFilter extends Filter {
+    
+    private boolean inverted = false;
     
     @Override
     public BitSet getFilteredRows(IrisStarTable table) {
-        return new BitSet();
+        BitSet bitSet = new BitSet();
+        if (inverted) {
+            // Filter all rows in the plotter table.
+            bitSet.set(0, (int) table.getPlotterTable().getRowCount());
+        }
+        return bitSet;
     }
     
+    @Override 
+    public void invert() {
+        inverted = !inverted;
+    }
 }
