1902c76bad48df848be47f16161a20b74902f9c2
Erik H <erik.holum@cfa.harvard.edu>
2016-03-21 17:22:20 -0400

    Filter as an interface, not abstraction.

--- iris-visualizer/src/main/java/cfa/vo/iris/visualizer/filters/Filter.java
+++ iris-visualizer/src/main/java/cfa/vo/iris/visualizer/filters/Filter.java
@@ -16,33 +16,9 @@
 package cfa.vo.iris.visualizer.filters;
 
 import java.util.BitSet;
-import java.util.UUID;
-
-import org.apache.commons.lang.StringUtils;
-
 import cfa.vo.iris.visualizer.stil.tables.IrisStarTable;
 
-public abstract class Filter {
-    
-    public final String id;
-    
-    public Filter() {
-        this.id = UUID.randomUUID().toString();
-    }
-    
-    @Override
-    public boolean equals(Object other) {
-        if (other == null || !(other instanceof Filter)) {
-            return false;
-        }
-        Filter o = (Filter) other;
-        return StringUtils.equals(id, o.id);
-    }
-    
-    @Override
-    public int hashCode() {
-        return id.hashCode();
-    }
+public interface Filter {
     
     public abstract BitSet getFilteredRows(IrisStarTable table);
     
