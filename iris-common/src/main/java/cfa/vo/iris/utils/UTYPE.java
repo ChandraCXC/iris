382fe3e206465f3ca227c84423242caf4fb6e01a
Erik H <erik.holum@cfa.harvard.edu>
2016-03-23 15:52:31 -0400

    Utype utility functions to a common place.

--- iris-common/src/main/java/cfa/vo/iris/utils/UTYPE.java
+++ iris-common/src/main/java/cfa/vo/iris/utils/UTYPE.java
@@ -1,8 +1,50 @@
 package cfa.vo.iris.utils;
 
-/**
- * Created by olaurino on 10/28/15.
- */
+import org.apache.commons.lang.StringUtils;
+
+import cfa.vo.sedlib.common.Utypes;
+
 public class UTYPE {
     public final static String FLUX_STAT_ERROR = "Spectrum.Data.FluxAxis.Accuracy.StatError";
+
+    
+    public static String trimPrefix(String utype) {
+        
+        // Remove prefixes if present
+        if (StringUtils.contains(utype, ":")) {
+            utype = utype.split(":")[1];
+        }
+        
+        return utype;
+    }
+    
+    public static boolean compareUtypes(String utype1, String utype2) {
+        
+        // If either of them are empty they are not equal
+        if (StringUtils.isEmpty(utype1) || StringUtils.isEmpty(utype2)) {
+            return false;
+        }
+        
+        // if the strings are equal then they are equals
+        if (StringUtils.equalsIgnoreCase(utype1, utype2)) {
+            return true;
+        }
+        
+        utype1 = trimPrefix(utype1);
+        utype2 = trimPrefix(utype2);
+        
+        // Use Utype comparisons
+        int u1 = Utypes.getUtypeFromString(utype1);
+        int u2 = Utypes.getUtypeFromString(utype2);
+        
+        if (Utypes.INVALID_UTYPE == u1 || Utypes.INVALID_UTYPE == u2) {
+            return false;
+        }
+        
+        if (u1 == u2) {
+            return true;
+        }
+        
+        return false;
+    }
 }
--- iris-visualizer/src/main/java/cfa/vo/iris/visualizer/stil/tables/UtypeColumnInfoMatcher.java
+++ iris-visualizer/src/main/java/cfa/vo/iris/visualizer/stil/tables/UtypeColumnInfoMatcher.java
@@ -18,7 +18,7 @@ package cfa.vo.iris.visualizer.stil.tables;
 
 import org.apache.commons.lang.StringUtils;
 
-import cfa.vo.sedlib.common.Utypes;
+import cfa.vo.iris.utils.UTYPE;
 import uk.ac.starlink.table.ColumnInfo;
 
 public class UtypeColumnInfoMatcher implements ColumnInfoMatcher {
@@ -26,7 +26,7 @@ public class UtypeColumnInfoMatcher implements ColumnInfoMatcher {
     @Override
     public boolean isCompatible(ColumnInfo c1, ColumnInfo c2) {
         
-        boolean utypesMatch = compareUtypes(c1.getUtype(), c2.getUtype());
+        boolean utypesMatch = UTYPE.compareUtypes(c1.getUtype(), c2.getUtype());
         if (utypesMatch) {
             return true;
         }
@@ -42,40 +42,4 @@ public class UtypeColumnInfoMatcher implements ColumnInfoMatcher {
         // If both utype and names are not equal
         return false;
     }
-    
-    private boolean compareUtypes(String utype1, String utype2) {
-        
-        // If either of them are empty they are not equal
-        if (StringUtils.isEmpty(utype1) || StringUtils.isEmpty(utype2)) {
-            return false;
-        }
-        
-        // if the strings are equal then they are equals
-        if (StringUtils.equalsIgnoreCase(utype1, utype2)) {
-            return true;
-        }
-        
-        // Remove prefixes if present
-        if (StringUtils.contains(utype1, ":")) {
-            utype1 = utype1.split(":")[1];
-        }
-        if (StringUtils.contains(utype2, ":")) {
-            utype2 = utype2.split(":")[1];
-        }
-        
-        // Use Utype comparisons
-        int u1 = Utypes.getUtypeFromString(utype1);
-        int u2 = Utypes.getUtypeFromString(utype2);
-        
-        if (Utypes.INVALID_UTYPE == u1 || Utypes.INVALID_UTYPE == u2) {
-            return false;
-        }
-        
-        if (u1 == u2) {
-            return true;
-        }
-        
-        return false;
-    }
-
 }
