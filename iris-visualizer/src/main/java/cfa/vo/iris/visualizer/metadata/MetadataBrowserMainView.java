24e2808cbb4de41f8ff206aebb2df111f6175af6
Erik H <erik.holum@cfa.harvard.edu>
2016-04-14 09:34:34 -0400

    Adding extract functionality to Metadata Browser.

--- iris-visualizer/src/main/java/cfa/vo/iris/visualizer/metadata/MetadataBrowserMainView.java
+++ iris-visualizer/src/main/java/cfa/vo/iris/visualizer/metadata/MetadataBrowserMainView.java
@@ -16,10 +16,12 @@
 package cfa.vo.iris.visualizer.metadata;
 
 import java.awt.Component;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -38,6 +40,8 @@ import cfa.vo.iris.visualizer.preferences.VisualizerListener;
 import cfa.vo.iris.visualizer.stil.tables.ColumnInfoMatcher;
 import cfa.vo.iris.visualizer.stil.tables.IrisStarTable;
 import cfa.vo.iris.visualizer.stil.tables.StackedStarTable;
+import cfa.vo.sedlib.common.SedInconsistentException;
+import cfa.vo.sedlib.common.SedNoDataException;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -177,6 +181,44 @@ public class MetadataBrowserMainView extends javax.swing.JInternalFrame {
         IrisStarTable selectedTable = selectedTables.get(starTableIndex);
         int index = IrisStarTable.getTableStartIndex(selectedStarTables, selectedTable);
         plotterStarJTable.selectRowIndex(index + irow);
+    }
+
+    /**
+     * Extracts the selected rows in the Metadata browser to a new SED, then adds the SED to the
+     * SedManager through the Iris Workspace.
+     * 
+     */
+    private void extractSelectionToSed() {
+        
+        // Do nothing if no SED is selected
+        if (selectedSed == null) {
+            JOptionPane.showMessageDialog(this, "No SED in browser. Please load an SED.");
+            return;
+        }
+        
+        // Do nothing if there are no rows selected
+        int[] selectedRows = plotterStarJTable.getSelectedRows();
+        if (selectedRows == null || selectedRows.length == 0) {
+            JOptionPane.showMessageDialog(this, "No rows selected to extract. Please select rows.");
+            return;
+        }
+        
+        // Extract selected rows to new Segments
+        SegmentExtractor extractor = new SegmentExtractor(selectedStarTables, selectedRows);
+        ExtSed newSed = new ExtSed(selectedSed.getId());
+        
+        // Build a new SED
+        try {
+            newSed.addSegment(extractor.getSegments());
+        } catch (SedInconsistentException | SedNoDataException e) {
+            logger.log(Level.SEVERE, "Could not extract segments from SED", e);
+            JOptionPane.showMessageDialog(this, "ERROR:" + e.getMessage(), 
+                    null, JOptionPane.ERROR_MESSAGE);
+            throw new RuntimeException(e);
+        }
+        
+        JOptionPane.showMessageDialog(this, "Adding new SED to workspace.");
+        ws.getSedManager().add(newSed);
     }
     
     
@@ -477,6 +519,11 @@ public class MetadataBrowserMainView extends javax.swing.JInternalFrame {
         fileMenu.setName("Extract"); // NOI18N
 
         extractToSedMenuItem.setText("Extract to New SED");
+        extractToSedMenuItem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                extractToSedMenuItemActionPerformed(evt);
+            }
+        });
         fileMenu.add(extractToSedMenuItem);
 
         broadcastToSampMenuItem.setText("Broadcast to SAMP");
@@ -600,6 +647,10 @@ public class MetadataBrowserMainView extends javax.swing.JInternalFrame {
         }
         table.clearSelection();
     }//GEN-LAST:event_clearSelectionButtonActionPerformed
+
+    private void extractToSedMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_extractToSedMenuItemActionPerformed
+        extractSelectionToSed();
+    }//GEN-LAST:event_extractToSedMenuItemActionPerformed
 
     private void selectAllButtonActionPerformed(
             java.awt.event.ActionEvent evt) {// GEN-FIRST:event_selectAllButtonActionPerformed
