ad5ba3968cabc6059fd5187b6ede7815ebeaec67
Erik H <erik.holum@cfa.harvard.edu>
2016-04-14 15:22:08 -0400

    Handle extractions using the preferences executor.

--- iris-visualizer/src/test/java/cfa/vo/iris/visualizer/metadata/MetadataBrowserMainViewTest.java
+++ iris-visualizer/src/test/java/cfa/vo/iris/visualizer/metadata/MetadataBrowserMainViewTest.java
@@ -370,7 +370,7 @@ public class MetadataBrowserMainViewTest extends AbstractComponentGUITest {
         sed.getSegment(0).createTarget();
         sed.getSegment(0).getTarget().setName(new TextParam("target1"));
         sed.getSegment(1).createTarget();
-        sed.getSegment(1).getTarget().setName(new TextParam("target1"));
+        sed.getSegment(1).getTarget().setName(new TextParam("target2"));
         
         sedManager.add(sed);
         
@@ -408,7 +408,7 @@ public class MetadataBrowserMainViewTest extends AbstractComponentGUITest {
             public Trigger process(Window warning) throws Exception {
                 // Check warning message
                 assertTrue(StringUtils.contains(warning.getTextBox("OptionPane.label").getText(), 
-                             "Added new SED"));
+                             "Added new Filter SED"));
                 return Trigger.DO_NOTHING;
             }
         }).run();
@@ -432,10 +432,10 @@ public class MetadataBrowserMainViewTest extends AbstractComponentGUITest {
         });
         
         // Select all segments
-        starTableList.selectIndices(0,1);
+        starTableList.selectIndices(1,0);
         
         // Select first rows from both segments
-        plotterTable.selectRows(0,3);
+        plotterTable.selectRows(3,0);
         
         // Should throw a success message
         makeExtractWindowInterceptor().process(new WindowHandler() {
@@ -443,7 +443,7 @@ public class MetadataBrowserMainViewTest extends AbstractComponentGUITest {
             public Trigger process(Window warning) throws Exception {
                 // Check warning message
                 assertTrue(StringUtils.contains(warning.getTextBox("OptionPane.label").getText(), 
-                             "Added new SED"));
+                             "Added new Filter SED"));
                 return Trigger.DO_NOTHING;
             }
         }).run();
