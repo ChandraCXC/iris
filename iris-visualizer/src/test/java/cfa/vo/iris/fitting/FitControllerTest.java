13dd2b41def33d607188f31b9149afd521748b1c
Omar Laurino <olaurino@cfa.harvard.edu>
2016-04-28 12:56:48 -0400

    add output for user models

--- iris-visualizer/src/test/java/cfa/vo/iris/fitting/FitControllerTest.java
+++ iris-visualizer/src/test/java/cfa/vo/iris/fitting/FitControllerTest.java
@@ -1,14 +1,13 @@
 package cfa.vo.iris.fitting;
 
 import cfa.vo.interop.SAMPFactory;
+import cfa.vo.iris.fitting.custom.CustomModelType;
 import cfa.vo.iris.fitting.custom.CustomModelsManager;
+import cfa.vo.iris.fitting.custom.DefaultCustomModel;
 import cfa.vo.iris.sed.ExtSed;
 import cfa.vo.sherpa.ConfidenceResults;
 import cfa.vo.sherpa.SherpaClient;
-import cfa.vo.sherpa.models.CompositeModel;
-import cfa.vo.sherpa.models.Model;
-import cfa.vo.sherpa.models.ModelFactory;
-import cfa.vo.sherpa.models.Parameter;
+import cfa.vo.sherpa.models.*;
 import cfa.vo.sherpa.optimization.OptimizationMethod;
 import cfa.vo.sherpa.stats.Statistic;
 import org.apache.commons.io.FileUtils;
@@ -17,8 +16,10 @@ import org.mockito.Mockito;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -40,7 +41,7 @@ public class FitControllerTest {
         assertEquals(getOutputString(), os.toString("UTF-8"));
     }
 
-    private FitConfiguration createFit() {
+    private FitConfiguration createFit() throws Exception {
         FitConfiguration fit = new FitConfiguration();
 
         ModelFactory factory = new ModelFactory();
@@ -57,8 +58,15 @@ public class FitControllerTest {
         m2.findParameter("gamma").setVal(0.01);
         m2.findParameter("ampl").setVal(0.02);
 
-        CompositeModel cm = SAMPFactory.get(CompositeModel.class);
-        cm.setName("m1+m2");
+        DefaultCustomModel userModel = Mockito.mock(DefaultCustomModel.class);
+        Model model = new ModelStub();
+        UserModel um = new UserModelStub();
+        Mockito.stub(userModel.makeModel(Mockito.anyString())).toReturn(model);
+        Mockito.stub(userModel.makeUserModel(Mockito.anyString())).toReturn(um);
+        fit.addUserModel(userModel, "m3");
+
+        CompositeModel cm = fit.getModel();
+        cm.setName("m1+m2+m3");
         cm.addPart(m);
         cm.addPart(m2);
 
@@ -89,5 +97,187 @@ public class FitControllerTest {
     private String getOutputString() throws Exception {
         String path = getClass().getResource("fit.output").getFile();
         return FileUtils.readFileToString(new File(path));
+    }
+
+
+    private class ModelStub implements Model {
+        private List<Parameter> pars;
+
+        public ModelStub() {
+            pars = new ArrayList<>();
+            pars.add(new ParameterStub("m3.p1", 0.1, 1));
+            pars.add(new ParameterStub("m3.p2", 0.2, 0));
+            pars.add(new ParameterStub("m3.p3", 0.3, 0));
+        }
+
+        @Override
+        public String getName() {
+            return "tablemodel.m3";
+        }
+
+        @Override
+        public void setName(String name) {
+
+        }
+
+        @Override
+        public String getDescription() {
+            return null;
+        }
+
+        @Override
+        public void setDescription(String description) {
+
+        }
+
+        @Override
+        public List<Parameter> getPars() {
+            return pars;
+        }
+
+        @Override
+        public void addPar(Parameter par) {
+
+        }
+
+        @Override
+        public Parameter findParameter(String paramName) {
+            return null;
+        }
+    }
+
+    private class ParameterStub implements Parameter {
+        private String name;
+        private Double val;
+        private Integer frozen;
+
+        public ParameterStub(String name, Double val, Integer frozen) {
+            this.name = name;
+            this.val = val;
+            this.frozen = frozen;
+        }
+
+        @Override
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public void setName(String name) {
+
+        }
+
+        @Override
+        public Double getVal() {
+            return val;
+        }
+
+        @Override
+        public void setVal(Double value) {
+
+        }
+
+        @Override
+        public Double getMin() {
+            return null;
+        }
+
+        @Override
+        public void setMin(Double min) {
+
+        }
+
+        @Override
+        public Double getMax() {
+            return null;
+        }
+
+        @Override
+        public void setMax(Double max) {
+
+        }
+
+        @Override
+        public Integer getFrozen() {
+            return frozen;
+        }
+
+        @Override
+        public void setFrozen(Integer frozen) {
+
+        }
+
+        @Override
+        public Integer getHidden() {
+            return null;
+        }
+
+        @Override
+        public void setHidden(Integer hidden) {
+
+        }
+
+        @Override
+        public Integer getAlwaysfrozen() {
+            return null;
+        }
+
+        @Override
+        public void setAlwaysfrozen(Integer alwaysfrozen) {
+
+        }
+
+        @Override
+        public String getUnits() {
+            return null;
+        }
+
+        @Override
+        public void setUnits(String units) {
+
+        }
+
+        @Override
+        public String getLink() {
+            return null;
+        }
+
+        @Override
+        public void setLink(String link) {
+
+        }
+    }
+
+    private class UserModelStub implements UserModel {
+
+        @Override
+        public String getName() {
+            return "function.m3";
+        }
+
+        @Override
+        public void setName(String name) {
+
+        }
+
+        @Override
+        public String getFile() {
+            return "file://somewhere/on/disk";
+        }
+
+        @Override
+        public void setFile(String path) {
+
+        }
+
+        @Override
+        public String getFunction() {
+            return "somefunc";
+        }
+
+        @Override
+        public void setFunction(String function) {
+
+        }
     }
 }
