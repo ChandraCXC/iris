23c1f5d449f5b849374a3ae41d931d1810a55f81
Erik H <erik.holum@cfa.harvard.edu>
2016-05-02 12:22:25 -0400

    Initial commit of cleaned up models.

---
+++ iris-visualizer/src/main/java/cfa/vo/iris/visualizer/preferences/VisualizerDataModel.java
@@ -0,0 +1,115 @@
+package cfa.vo.iris.visualizer.preferences;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.LinkedList;
+import java.util.List;
+
+import cfa.vo.iris.sed.ExtSed;
+import cfa.vo.iris.visualizer.stil.tables.IrisStarTable;
+
+/**
+ * Dynamic model for the plotter and metadata browser. Maintains the current state
+ * of the Visualizer component.
+ *
+ */
+public class VisualizerDataModel {
+    
+    public static final String PROP_SELECTED_SEDS = "selectedSeds";
+    public static final String PROP_SELECTED_SEGMENT_MODELS = "selectedSegmentModels";
+    public static final String PROP_SELECTED_TABLES = "selectedTables";
+    public static final String PROP_SELECTED_STARTABLES = "selectedStarTables";
+
+    private final PropertyChangeSupport pcs;
+    private final VisualizerDataStore store;
+
+    // Seds to display in the visualizer
+    private List<ExtSed> selectedSeds = new LinkedList<>();
+    
+    // list of star tables associated with selectedSed, these tables will all be plotted
+    List<SegmentModel> selectedSegmentModels = new LinkedList<>();
+    
+    // list of star tables associated with selectedSed, these tables will all be plotted
+    List<IrisStarTable> sedStarTables = new LinkedList<>();
+    
+    // list of selected StarTables from selectedTables, or which star tables are shown in the 
+    // Metadata browser
+    List<IrisStarTable> selectedStarTables = new LinkedList<>();
+    
+    public VisualizerDataModel(VisualizerDataStore store) {
+        this.store = store;
+        this.pcs = new PropertyChangeSupport(this);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        this.pcs.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        this.pcs.removePropertyChangeListener(listener);
+    }
+    
+    /*
+     * 
+     * Getters and Setters
+     * 
+     */
+    
+    public List<ExtSed> getSelectedSeds() {
+        return selectedSeds;
+    }
+
+    public void setSelectedSeds(List<ExtSed> selectedSeds) {
+        List<ExtSed> oldList = this.selectedSeds;
+        this.selectedSeds = selectedSeds;
+        
+        // Update models
+        List<SegmentModel> newModels = new LinkedList<>();
+        List<IrisStarTable> newTables = new LinkedList<>();
+        
+        for (ExtSed sed : selectedSeds) {
+            SedModel sedModel = store.getSedPreferences(sed);
+            for (int i=0; i<sed.getNumberOfSegments(); i++) {
+                SegmentModel segModel = sedModel.getSegmentPreferences(sed.getSegment(i));
+                newModels.add(segModel);
+                newTables.add(segModel.getInSource());
+            }
+        }
+        this.setSelectedSegmentModels(newModels);
+        this.setSedStarTables(newTables);
+        
+        pcs.firePropertyChange(PROP_SELECTED_SEDS, oldList, selectedSeds);
+    }
+    
+    public List<SegmentModel> getSelectedSegmentModels() {
+        return selectedSegmentModels;
+    }
+    
+    // Locked down since these are tied to the selected seds
+    void setSelectedSegmentModels(List<SegmentModel> newModels) {
+        List<SegmentModel> oldTables = this.selectedSegmentModels;
+        this.selectedSegmentModels = newModels;
+        pcs.firePropertyChange(PROP_SELECTED_SEGMENT_MODELS, oldTables, newModels);
+    }
+    
+    public List<IrisStarTable> getSelectedTables() {
+        return sedStarTables;
+    }
+    
+    // Locked down since these are tied to the selected seds
+    void setSedStarTables(List<IrisStarTable> newTables) {
+        List<IrisStarTable> oldTables = sedStarTables;
+        this.sedStarTables = newTables;
+        pcs.firePropertyChange(PROP_SELECTED_TABLES, oldTables, sedStarTables);
+    }
+    
+    public List<IrisStarTable> getSelectedStarTables() {
+        return selectedStarTables;
+    }
+
+    public void setSelectedStarTables(List<IrisStarTable> newStarTables) {
+        List<IrisStarTable> oldStarTables = selectedStarTables;
+        this.selectedStarTables = newStarTables;
+        pcs.firePropertyChange(PROP_SELECTED_STARTABLES, oldStarTables, newStarTables);
+    }
+}
---
+++ iris-visualizer/src/main/java/cfa/vo/iris/visualizer/preferences/VisualizerDataStore.java
@@ -0,0 +1,137 @@
+package cfa.vo.iris.visualizer.preferences;
+
+import java.util.Collections;
+import java.util.IdentityHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import cfa.vo.iris.sed.ExtSed;
+import cfa.vo.iris.visualizer.stil.tables.IrisStarTableAdapter;
+import cfa.vo.sedlib.Segment;
+
+/**
+ * 'Persistence' layer for all SED data related to the Iris Visualizer.
+ *
+ */
+public class VisualizerDataStore {
+    
+    private static final ExecutorService visualizerExecutor = Executors.newFixedThreadPool(5);
+    
+    // Converts a segment into an Iris StarTable
+    IrisStarTableAdapter adapter;
+
+    // All preferences for each ExtSed in the workspace
+    final Map<ExtSed, SedModel> sedPreferences;
+    
+    public VisualizerDataStore() {
+        this.adapter = new IrisStarTableAdapter(visualizerExecutor);
+        this.sedPreferences = Collections.synchronizedMap(new IdentityHashMap<ExtSed, SedModel>());
+    }
+
+    /**
+     * @return Preferences map for each SED.
+     */
+    public Map<ExtSed, SedModel> getSedPreferences() {
+        return sedPreferences;
+    }
+    
+    /**
+     * @return Preferences for the given SED
+     */
+    public SedModel getSedPreferences(ExtSed sed) {
+        return sedPreferences.get(sed);
+    }
+
+    /**
+     * Adds or updates the SED to the preferences map.
+     * @param sed
+     */
+    public void update(ExtSed sed) {
+        if (sedPreferences.containsKey(sed)) {
+            sedPreferences.get(sed).refresh();
+        } else {
+            sedPreferences.put(sed, new SedModel(sed, adapter));
+        }
+    }
+    
+    /**
+     * Adds or updates the segment within the specified SED.
+     * @param sed - the sed to which the segment is attached
+     * @param segment
+     */
+    public void update(ExtSed sed, Segment segment) {
+        // Do nothing for null segments
+        if (segment == null) return;
+        
+        if (sedPreferences.containsKey(sed)) {
+            sedPreferences.get(sed).addSegment(segment);
+        } else {
+            // The segment will automatically be serialized and attached the the 
+            // SedPrefrences since it's assumed to be attached to the SED.
+            sedPreferences.put(sed, new SedModel(sed, adapter));
+        }
+    }
+    
+    /**
+     * Adds or updates the segments within the specified SED.
+     * @param sed - the sed to which the segment is attached
+     * @param segments - the list of segments to add
+     */
+    public void update(ExtSed sed, List<Segment> segments) {
+        if (sedPreferences.containsKey(sed)) {
+            for (Segment segment : segments) {
+                // Do nothing for null segments
+                if (segment == null) continue;
+                sedPreferences.get(sed).addSegment(segment);
+            }
+        } else {
+            // The segment will automatically be serialized and attached the the 
+            // SedPrefrences since it's assumed to be attached to the SED.
+            sedPreferences.put(sed, new SedModel(sed, adapter));
+        }
+    }
+    
+    /**
+     * Removes the SED from the preferences map.
+     * @param sed
+     */
+    public void remove(ExtSed sed) {
+        if (!sedPreferences.containsKey(sed)) {
+            return;
+        }
+        sedPreferences.get(sed).removeAll();
+        sedPreferences.remove(sed);
+        //fire(sed, VisualizerCommand.RESET);
+    }
+    
+    /**
+     * Removes the segment from the specified Sed Preferences map.
+     * @param sed
+     * @param segment
+     */
+    public void remove(ExtSed sed, Segment segment) {
+        // Do nothing for null segments
+        if (segment == null) return;
+        
+        if (sedPreferences.containsKey(sed)) {
+            sedPreferences.get(sed).removeSegment(segment);
+        }
+    }
+    
+    /**
+     * Removes the segments from the specified Sed Preferences map.
+     * @param sed
+     * @param segments
+     */
+    public void remove(ExtSed sed, List<Segment> segments) {
+        if (sedPreferences.containsKey(sed)) {
+            for (Segment segment : segments) {
+                // Do nothing for null segments
+                if (segment == null) continue;
+                sedPreferences.get(sed).removeSegment(segment);
+            }
+        }
+    }
+}
