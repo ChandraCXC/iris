3e9fa0d4104d517b81944e1e1809a83f78d9c933
Jamie Budynkiewicz <jbudynkiewicz@cfa.harvard.edu>
2016-05-04 18:51:18 -0400

    Add basic filter expression parsing.
    Columns are specified by their column index (1, 2, 3, ... N) and must be
    preceeded by a "$". Validator.process() returns an array of int's that
    correspond to the filtered rows. Currently, only double columns are supported
    (no strings). Comparison/relational operators (<, <=, ==, etc.) are
    implemented. AND, OR, ANDNOT logical operators have not yet been implemented.
    
    TODOS:
     - handle strings
     - add AND, OR, ANDNOT operators
     - handle non-valid entries

--- iris-visualizer/src/test/java/cfa/vo/iris/visualizer/metadata/FilterExpressionValidatorTest.java
+++ iris-visualizer/src/test/java/cfa/vo/iris/visualizer/metadata/FilterExpressionValidatorTest.java
@@ -17,12 +17,14 @@ package cfa.vo.iris.visualizer.metadata;
 
 import cfa.vo.iris.sed.stil.SegmentStarTable;
 import cfa.vo.iris.test.unit.TestUtils;
+import cfa.vo.iris.visualizer.stil.IrisStarJTable;
 import cfa.vo.iris.visualizer.stil.tables.IrisStarTable;
 import cfa.vo.iris.visualizer.stil.tables.IrisStarTableAdapter;
 import cfa.vo.sedlib.Segment;
 import java.util.List;
 import java.util.concurrent.Executors;
 import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.Test;
 import uk.ac.starlink.table.StarTable;
 
@@ -34,28 +36,107 @@ public class FilterExpressionValidatorTest {
     
     private IrisStarTableAdapter adapter = new IrisStarTableAdapter(Executors.newSingleThreadExecutor());
     private FilterExpressionValidator validator;
+    IrisStarJTable jTable;
     
     public FilterExpressionValidatorTest() {
         
     }
-
-    @Test
-    public void testSimpleExpression() throws Exception {
-        
+    
+    @Before
+    public void setUp() throws Exception {
+        // setup table to filter
         double[] x = new double[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
         double[] y = x;
-        
         Segment seg = TestUtils.createSampleSegment(x, y);
         
         IrisStarTable table = adapter.convertSegment(seg);
+        jTable = new IrisStarJTable();
+        jTable.setStarTable(table);
         
-        SegmentStarTable plotterTable = table.getPlotterTable();
-        StarTable dataTable = table.getSegmentDataTable();
+        // filter validator
+        validator = new FilterExpressionValidator(jTable);
+    }
+    
+
+    @Test
+    public void testSimpleExpression() {
         
-        // get all the points whose spectral value is greater than 5
-        String expression = "$1 > 5";
+        String expression; // filter expression
         
+        // get all the points whose value is equal to 5
+        expression = "$1 == 5";
         
+        // the evaluator returns the array of rows whose data complies
+        // with the filter expression
+        assertArrayEquals(new int[]{4}, validator.process(expression));
+        
+        // get all points whose value is not equal to 5
+        expression = "$1 != 5";
+        assertArrayEquals(new int[]{0, 1, 2, 3, 5, 6, 7, 8, 9, 10}, 
+                validator.process(expression));
+        
+        // get all points whose value is less than 5
+        expression = "$1 < 5";
+        assertArrayEquals(new int[]{0, 1, 2, 3}, 
+                validator.process(expression));
+        
+        // get all points whose value is greater than 5
+        expression = "$1 > 5";
+        assertArrayEquals(new int[]{5, 6, 7, 8, 9, 10}, 
+                validator.process(expression));
+        
+        // get all points whose value is less than or equal to 5
+        expression = "$1 <= 5";
+        assertArrayEquals(new int[]{0, 1, 2, 3, 4}, 
+                validator.process(expression));
+        
+        // get all points whose value is greater than or equal to 5
+        expression = "$1 >= 5";
+        assertArrayEquals(new int[]{4, 5, 6, 7, 8, 9, 10}, 
+                validator.process(expression));
+        
+    }
+    
+    @Test
+    public void testMoreComplicated() {
+        String expression; // filter expression
+        
+        // get all the points whose value is < 6
+        expression = "$1 + 2 < 6";
+        
+        // the evaluator returns the array of rows whose data complies
+        // with the filter expression
+        assertArrayEquals(new int[]{0, 1, 2}, validator.process(expression));
+        
+        // get all the points whose value is greater than or equal to 10
+        expression = "$2*3 -6 >= 10 - 1";
+        assertArrayEquals(new int[]{4, 5, 6, 7, 8, 9, 10}, 
+                validator.process(expression));
+        
+        //
+        // test comparing values in different columns
+        //
+        
+        // should give all matching rows since $1 and $2 are equal
+        expression = "$1 == $2";
+        assertArrayEquals(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 
+                validator.process(expression));
+        
+        expression = "$2 *2 !=$1";
+        assertArrayEquals(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 
+                validator.process(expression));
+        
+        expression = "$2 != $1";
+        assertArrayEquals(new int[]{}, 
+                validator.process(expression));
+        
+        expression = "$1 *4 < $2+10";
+        assertArrayEquals(new int[]{0, 1, 2}, 
+                validator.process(expression));
+        
+        expression = "$1 *4 > $2/2 + 10";
+        assertArrayEquals(new int[]{2, 3, 4, 5, 6, 7, 8, 9, 10}, 
+                validator.process(expression));
     }
     
     @Test
