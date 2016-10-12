bbd2fabe022df5ebc3f798fd3ba0809a13ad43d0
Jamie Budynkiewicz <jbudynkiewicz@cfa.harvard.edu>
2016-10-12 13:50:25 -0400

    For #334: increment legend labels with segment table IDs in MB

--- iris-visualizer/src/main/java/cfa/vo/iris/visualizer/preferences/VisualizerDataModel.java
+++ iris-visualizer/src/main/java/cfa/vo/iris/visualizer/preferences/VisualizerDataModel.java
@@ -344,6 +344,7 @@ public class VisualizerDataModel {
         
         model.getInSource().setName(id);
         model.setSuffix(id);
+        model.setLabel(id);
         labels.add(id);
     }
     
